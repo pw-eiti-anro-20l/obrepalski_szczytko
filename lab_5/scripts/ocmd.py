0464488ef8a8ecd83f9cf3cf789c9b5939c36ee3
Scythio <szczytek.pl@gmail.com>
2020-05-24 22:37:21 +0200

    com3 added elipse test

--- lab_5/scripts/ocmd.py
+++ lab_5/scripts/ocmd.py
@@ -2,7 +2,7 @@
 
 from lab_5.srv import ocmd_control, oint_control
 from sensor_msgs.msg import JointState
-
+from math import pi, sin, cos
 from time import sleep
 from sys import stderr
 import rospy
@@ -28,12 +28,23 @@ def handle(req):
                 n+=1
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
-            
-            
-            
+                    
             
     elif req.mode == 'elipse':
-        print('Elipse')
+        x=req.x
+        y=req.y
+        z=req.z
+        a=req.a
+        b=req.b
+        T=1
+        rospy.wait_for_service('oint_control_srv')
+        make_elipse = rospy.ServiceProxy('oint_control_srv', oint_control)
+        task=make_elipse('poly',x+a,y,z,0,0,0,4*T)
+        alpha = 0.0
+        while alpha < 2*pi*req.t :
+            alpha += 2*pi/20
+            P=[x+a*cos(alpha),y+b*sin(alpha)]
+            task=make_elipse('lin',P[0],P[1],z,0,0,0,T)
     else:
         print('Type \'square\' or \'elipse\'')
         return "Type \'square\' or \'elipse\' \'a\' \'b\' \'x\' \'y\' \'z\' \'t\'"
